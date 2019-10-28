e5154086f29081e23601265447169eacfee6a488
Nicolas Caceda <nicolas.caceda@gmail.com>
2019-10-28 18:36:07 -0400

    Add project files.

---
+++ DanceApp/DanceApp/View/LessonView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DanceApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LessonView : ContentPage
+    {
+        public LessonView()
+        {
+            InitializeComponent();
+        }
+
+        private void Return_To_Main(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
