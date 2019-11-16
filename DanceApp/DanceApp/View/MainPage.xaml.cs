3b163178402a0049887ab1ce81668d7531b49111
Nicolas Caceda <nicolas.caceda@gmail.com>
2019-11-16 12:28:54 -0500

    Made Changes Kamp Made

--- DanceApp/DanceApp/View/MainPage.xaml.cs
+++ DanceApp/DanceApp/View/MainPage.xaml.cs
@@ -12,6 +12,10 @@ namespace DanceApp.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        int show_grid = 1;
+        private bool _FirstGridShow = true;
+        private bool _SecondGridShow, _ThirdGridShow = false;
+
         public MainPage()
         {
             InitializeComponent();
@@ -20,6 +24,51 @@ namespace DanceApp.View
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new LessonView());
+        }
+
+        private void SwipeRight(object sender, SwipedEventArgs e)
+        {
+            if (show_grid == 1)
+                return;
+            show_grid--;
+
+            ToggleGrid(sender);
+        }
+
+        private void SwipeLeft(object sender, SwipedEventArgs e)
+        {
+            if (show_grid == 3)
+                return;
+            show_grid++;
+
+            ToggleGrid(sender);
+        }
+
+        private void ToggleGrid(object sender)
+        {
+            var grid = (StackLayout)sender;
+
+            switch (show_grid)
+            {
+                case 1:
+                    first_grid.IsVisible = true;
+                    second_grid.IsVisible = false;
+                    third_grid.IsVisible = false;
+                    break;
+                case 2:
+                    first_grid.IsVisible = false;
+                    second_grid.IsVisible = true;
+                    third_grid.IsVisible = false;
+                    break;
+                case 3:
+                    first_grid.IsVisible = false;
+                    second_grid.IsVisible = false;
+                    third_grid.IsVisible = true;
+                    break;
+                default: break;
+
+
+            }
         }
     }
 }
