fe2b20963e5e74881bf75c6a801b338e20de886a
Nicolas Caceda <nicolas.caceda@gmail.com>
2019-11-17 15:40:10 -0500

    Image now changes when ImageButton is pressed. Need to do videos now.

--- DanceApp/DanceApp/ViewModels/LessonViewModel.cs
+++ DanceApp/DanceApp/ViewModels/LessonViewModel.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 using DanceApp.Models;
+using Xamarin.Forms;
+
 
 namespace DanceApp.ViewModels
 {
-    class LessonViewModel
+    class LessonViewModel : INotifyPropertyChanged
     {
         private int Key;
-        public Lesson CurrentLesson{ get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Lesson CurrentLesson { get; set; }
+        public ICommand ChangeMainDisplay { protected set; get; }
+
+        private string displayURL = "tempimage.png";
+
+        public string DisplayURL
+        {
+            get { return displayURL; }
+            set
+            {
+                if (displayURL != value)
+                {
+                    displayURL = value;
+
+                    if(PropertyChanged != null)
+                    {
+                        Console.WriteLine($"Prop CHanged");
+                        PropertyChanged(this, new PropertyChangedEventArgs("DisplayURL"));
+                    }
+                }
+            }
+        }
+
 
         public LessonViewModel()
         {
@@ -30,6 +59,13 @@ namespace DanceApp.ViewModels
                 WomanFirstPath = "women",
                 WomanSecondPath = "women2"
             };
+
+            //TODO removex with {CurrentLesson.Key}
+            ChangeMainDisplay = new Command<string>((ButtonValue) =>
+            {
+                DisplayURL = $"Mx{ButtonValue}.png";
+            });
+
         }
 
     }
