ff910868896e2d9e92667cedc270e953f9e92c81
Nicolas Caceda <nicolas.caceda@gmail.com>
2019-11-18 21:15:34 -0500

    Im Helping Find A solution to this problem. And I implemented https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/custom-renderer/video-player/

---
+++ DanceApp/DanceApp.Android/VideoPlayerRenderer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ARelativeLayout = Android.Widget.RelativeLayout;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using DanceApp.Renderers.VideoPlayerRenderer;
+
+[assembly: ExportRenderer(typeof(VideoPlayer),
+                          typeof(DanceApp.Droid.VideoPlayerRenderer))]
+
+namespace DanceApp.Droid
+{
+    public class VideoPlayerRenderer : ViewRenderer<VideoPlayer, ARelativeLayout>
+    {
+        VideoView videoView;
+        MediaController mediaController;
+        bool isPrepared;
+        public VideoPlayerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<VideoPlayer> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    videoView = new VideoView(Context);
+
+                    ARelativeLayout relativeLayout = new ARelativeLayout(Context);
+                    relativeLayout.AddView(videoView);
+
+                    ARelativeLayout.LayoutParams layoutParams =
+                        new ARelativeLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
+                    layoutParams.AddRule(LayoutRules.CenterInParent);
+                    videoView.LayoutParameters = layoutParams;
+
+                    videoView.Prepared += OnVideoViewPrepared;
+
+                    SetNativeControl(relativeLayout);
+                }
+                SetSource();
+            }
+        }
+
+        void SetSource()
+        {
+            isPrepared = false;
+            bool hasSetSource = false;
+
+            if (Element.Source is UriVideoSource)
+            {
+                string uri = (Element.Source as UriVideoSource).Uri;
+
+                if (!String.IsNullOrWhiteSpace(uri))
+                {
+                    videoView.SetVideoURI(Android.Net.Uri.Parse(uri));
+                    hasSetSource = true;
+                }
+            }
+
+            if (hasSetSource && Element.AutoPlay)
+            {
+                videoView.Start();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (Control != null && videoView != null)
+            {
+                videoView.Prepared -= OnVideoViewPrepared;
+            }
+            base.Dispose(disposing);
+        }
+
+        void OnVideoViewPrepared(object sender, EventArgs args)
+        {
+            isPrepared = true;
+        }
+    }
+}
